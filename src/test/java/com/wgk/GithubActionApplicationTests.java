81a035b515d4ea69013c1c714485aeab74073914
wgk <2287334379@qq.com>
2025-02-22 20:30:32 +0800

    Initial commit

---
+++ src/test/java/com/wgk/GithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
